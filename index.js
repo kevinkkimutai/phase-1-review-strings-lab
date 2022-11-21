540294c37f512d441a74547f66b0b958ebd8fef0
kevinkkimutai <kiruikevinkim@gmail.com>
2022-11-21 17:51:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = "Kelvin";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = `Welcome, ${firstInitial}!`;
